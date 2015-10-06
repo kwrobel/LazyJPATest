fc633f6dcdbf80354be9a2ae5c745bef66222f00
Kay Wrobel <kay.wrobel@gmx.net>
2015-10-06 10:29:21 -0500

    Initial commit.

---
+++ src/lazyjpatest/LazyJPATest.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lazyjpatest;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import lazyjpatest.entity.Customer;
+import lazyjpatest.entity.DiscountCode;
+import lazyjpatest.entity.PurchaseOrder;
+
+/**
+ *
+ * @author kuw
+ */
+public class LazyJPATest {
+
+    private List<Customer> customers;
+    private List<DiscountCode> discountCodes;
+    private EntityManager em;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LazyJPATest test = new LazyJPATest();
+        test.getCustomers();
+        test.printCustomers();
+        System.exit(0);
+    }
+
+    public LazyJPATest() {
+        // NOTE: - Use LazyJPAOpenJPATestPU   for testing persistence unit using OpenJPA   JPA provider
+        //       - Use LazyJPAHibernateTestPU for testing persistence unit using Hibernate JPA provider
+        this.em = Persistence.createEntityManagerFactory("LazyJPAHibernateTestPU").createEntityManager();
+    }
+    public void getCustomers() {
+        TypedQuery<Customer> customersQuery = em.createNamedQuery("Customer.findAll", Customer.class);
+        try {
+            customers = customersQuery.getResultList();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void printCustomers() {
+        if (this.customers != null) {
+            System.out.println("CUSTOMERS");
+            System.out.println("==========================================");
+
+            for (Customer customer : this.customers) {
+                DiscountCode dc = customer.getDiscountCode();
+                List<PurchaseOrder> purchaseOrders = customer.getPurchaseOrderList();
+                System.out.print("ID: " + customer.getCustomerId());
+                System.out.print(", Name: " + customer.getName());
+                System.out.print(", Email: " + customer.getEmail());
+                System.out.print(", Discount: " + dc.getRate() + "%");
+                System.out.println("");
+                if (purchaseOrders != null && purchaseOrders.size() > 0) {
+                    for (PurchaseOrder po : purchaseOrders) {
+                        System.out.print("   --> Order#: " + po.getOrderNum());
+                        System.out.print(", Product: " + po.getProductId().getDescription());
+                        System.out.print(" (" + po.getProductId().getProductCode().getDescription() + ")");
+                        System.out.print(", Qty: " + po.getQuantity());
+                        System.out.println("");
+                    }
+                } else {
+                    System.out.println("    --> No purchase orders found!");
+                }
+            }
+        }
+    }    
+}
